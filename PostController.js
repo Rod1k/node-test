e1c8f3cd374c25f9963d84efbce27012764a4771
Rodion Otyrba <19rodion97@gmail.com>
2023-01-18 20:22:14 +0300

    commit1

--- PostController.js
+++ PostController.js
@@ -1,10 +1,10 @@
 import Post from "./Post.js";
+import PostService from "./PostService.js";
 
 class PostController {
     async create(req, res) {
         try {
-            const {author, title, content, picture} = req.body
-            const post = await Post.create({author, title, content, picture})
+            const post = await PostService.create(req.body);
             res.status(200).json(post);
         } catch (e) {
             res.status(500).json(e);
@@ -12,19 +12,39 @@ class PostController {
     }
 
     async getAll(req, res) {
-
+        try {
+            const posts = await PostService.getAll();
+            return res.json(posts);
+        } catch (e) {
+            res.status(500).json(e);
+        }
     }
 
     async getOne(req, res) {
-
+        try {
+            const post = await PostService.getOne(req.params.id);
+            return res.json(post);
+        } catch (e) {
+            res.status(500).json(e);
+        }
     }
 
     async update(req, res) {
-
+        try {
+            const updatedPost = await PostService.update(req.body);
+            return res.json(updatedPost);
+        } catch (e) {
+            res.status(500).json(e.message);
+        }
     }
 
     async delete(req, res) {
-
+        try {
+            const post = await PostService.delete(req.params.id);
+            return res.json(post);
+        } catch (e) {
+            res.status(500).json(e);
+        }
     }
 }
 
